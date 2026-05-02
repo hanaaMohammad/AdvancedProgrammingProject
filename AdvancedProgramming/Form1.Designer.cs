6bd82c8affc87c0d20933ca51354d7f8181b778e
Hana <hanaamhamad666@gmail.com>
2026-05-02 13:05:28 +0300

    Fix build errors in Form1.Designer.cs and Form1.cs

--- AdvancedProgramming/Form1.Designer.cs
+++ AdvancedProgramming/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +15,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.lblUsername = new System.Windows.Forms.Label();
@@ -125,8 +114,5 @@
         private System.Windows.Forms.Button btnAddUser;
         private System.Windows.Forms.Button btnViewUsers;
         private System.Windows.Forms.DataGridView dgvUsers;
-
-        #endregion
     }
 }
-
