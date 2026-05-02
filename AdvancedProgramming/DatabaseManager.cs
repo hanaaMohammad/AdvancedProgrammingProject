9e69009c26b215ec0fe3c8a0b22898f17791ee63
Hana <hanaamhamad666@gmail.com>
2026-05-02 13:14:22 +0300

    Revert Form1 changes and add admin user on database initialization

--- AdvancedProgramming/DatabaseManager.cs
+++ AdvancedProgramming/DatabaseManager.cs
@@ -26,6 +26,18 @@ namespace AdvancedProgramming
                         );";
                     using (var command = new SQLiteCommand(createTableSql, connection))
                         command.ExecuteNonQuery();
+
+                    string checkAdmin = "SELECT COUNT(*) FROM users WHERE username = 'admin'";
+                    using (var checkCmd = new SQLiteCommand(checkAdmin, connection))
+                    {
+                        long count = (long)checkCmd.ExecuteScalar();
+                        if (count == 0)
+                        {
+                            string addAdmin = "INSERT INTO users (username, password) VALUES ('admin', 'admin')";
+                            using (var adminCmd = new SQLiteCommand(addAdmin, connection))
+                                adminCmd.ExecuteNonQuery();
+                        }
+                    }
                 }
             }
             catch (Exception ex)
@@ -33,52 +45,6 @@ namespace AdvancedProgramming
                 File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db_error.txt"), ex.ToString());
                 throw;
             }
-        }
-
-        public static bool AddUser(string username, string password)
-        {
-            try
-            {
-                using (var connection = new SQLiteConnection(ConnectionString))
-                {
-                    connection.Open();
-                    string sql = "INSERT INTO users (username, password) VALUES (@username, @password)";
-                    using (var command = new SQLiteCommand(sql, connection))
-                    {
-                        command.Parameters.AddWithValue("@username", username);
-                        command.Parameters.AddWithValue("@password", password);
-                        command.ExecuteNonQuery();
-                        return true;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db_error.txt"), ex.ToString());
-                return false;
-            }
-        }
-
-        public static System.Data.DataTable GetAllUsers()
-        {
-            var table = new System.Data.DataTable();
-            try
-            {
-                using (var connection = new SQLiteConnection(ConnectionString))
-                {
-                    connection.Open();
-                    string sql = "SELECT id, username, created_at FROM users ORDER BY id";
-                    using (var adapter = new SQLiteDataAdapter(sql, connection))
-                    {
-                        adapter.Fill(table);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                File.WriteAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "db_error.txt"), ex.ToString());
-            }
-            return table;
         }
     }
 }
--- AdvancedProgramming/Form1.Designer.cs
+++ AdvancedProgramming/Form1.Designer.cs
@@ -2,8 +2,15 @@
 {
     partial class Form1
     {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -15,104 +22,26 @@
 
         #region Windows Form Designer generated code
 
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
-            this.lblUsername = new System.Windows.Forms.Label();
-            this.txtUsername = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.txtPassword = new System.Windows.Forms.TextBox();
-            this.btnAddUser = new System.Windows.Forms.Button();
-            this.btnViewUsers = new System.Windows.Forms.Button();
-            this.dgvUsers = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // lblUsername
-            // 
-            this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(20, 20);
-            this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(60, 20);
-            this.lblUsername.Text = "Username:";
-            // 
-            // txtUsername
-            // 
-            this.txtUsername.Location = new System.Drawing.Point(90, 17);
-            this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(150, 26);
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(260, 20);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(58, 20);
-            this.lblPassword.Text = "Password:";
-            // 
-            // txtPassword
-            // 
-            this.txtPassword.Location = new System.Drawing.Point(325, 17);
-            this.txtPassword.Name = "txtPassword";
-            this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(150, 26);
-            // 
-            // btnAddUser
-            // 
-            this.btnAddUser.Location = new System.Drawing.Point(490, 16);
-            this.btnAddUser.Name = "btnAddUser";
-            this.btnAddUser.Size = new System.Drawing.Size(100, 28);
-            this.btnAddUser.Text = "Add User";
-            this.btnAddUser.UseVisualStyleBackColor = true;
-            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
-            // 
-            // btnViewUsers
-            // 
-            this.btnViewUsers.Location = new System.Drawing.Point(600, 16);
-            this.btnViewUsers.Name = "btnViewUsers";
-            this.btnViewUsers.Size = new System.Drawing.Size(100, 28);
-            this.btnViewUsers.Text = "View Users";
-            this.btnViewUsers.UseVisualStyleBackColor = true;
-            this.btnViewUsers.Click += new System.EventHandler(this.btnViewUsers_Click);
-            // 
-            // dgvUsers
-            // 
-            this.dgvUsers.AllowUserToAddRows = false;
-            this.dgvUsers.AllowUserToDeleteRows = false;
-            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvUsers.Location = new System.Drawing.Point(20, 60);
-            this.dgvUsers.Name = "dgvUsers";
-            this.dgvUsers.ReadOnly = true;
-            this.dgvUsers.Size = new System.Drawing.Size(760, 350);
-            this.dgvUsers.TabIndex = 5;
-            // 
-            // Form1
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblUsername);
-            this.Controls.Add(this.txtUsername);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.txtPassword);
-            this.Controls.Add(this.btnAddUser);
-            this.Controls.Add(this.btnViewUsers);
-            this.Controls.Add(this.dgvUsers);
-            this.Name = "Form1";
-            this.Text = "Users Database Manager";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			this.SuspendLayout();
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Name = "Form1";
+			this.Text = "stop";
+			this.ResumeLayout(false);
+
         }
 
         #endregion
-
-        private System.Windows.Forms.Label lblUsername;
-        private System.Windows.Forms.TextBox txtUsername;
-        private System.Windows.Forms.Label lblPassword;
-        private System.Windows.Forms.TextBox txtPassword;
-        private System.Windows.Forms.Button btnAddUser;
-        private System.Windows.Forms.Button btnViewUsers;
-        private System.Windows.Forms.DataGridView dgvUsers;
     }
 }
+
