dbd034b8d156258eff542ff40e9db1099c6a1689
Hana <hanaamhamad666@gmail.com>
2026-05-09 16:11:28 +0300

    the FirstFormPageHome

--- AdvancedProgramming/Forms/LogInForm.cs
+++ AdvancedProgramming/Forms/LogInForm.cs
@@ -14,6 +14,7 @@ namespace AdvancedProgramming.Forms
         private bool passwordVasibilty = false;
         private Label Massage;
         private Toolbar toolbar;
+        
 
         public LogInForm()
         {
