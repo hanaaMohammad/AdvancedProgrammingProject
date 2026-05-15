3cb4cc5c789f5590546c16ffe0d2fc1674306a77
Hana <hanaamhamad666@gmail.com>
2026-05-15 15:20:15 +0300

    CodeRun

---
+++ AdvancedProgramming/Service/CodeRunner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedProgramming.Service
+{
+    public class CodeRunner
+    {
+       public string Run()
+        {
+            var path=new FileManger().Path("code", "c++");
+
+
+
+            return "";
+        }
+
+
+
+
+    }
+}
